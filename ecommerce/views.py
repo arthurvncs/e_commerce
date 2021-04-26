b4e66539b58bfbf57ea41045220279bbc2740495
arthurvncs <arthurxd.vncs@gmail.com>
2021-04-26 00:30:46 -0300

    Adicionando forms e modelando página de contato.

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .forms import ContactForm
+
+
 def home_page(request):
     context = {
         "title": "Página principal",
         "content": "Bem-vindo a página principal"
     }
     return render(request, "home_page.html", context)
+
 
 def about_page(request):
     context = {
@@ -14,9 +19,15 @@ def about_page(request):
     }
     return render(request, "about/view.html", context)
 
+
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
+
         "title": "Página de contato",
-        "content": "Bem-vindo a página de contato"
+        "content": "Bem-vindo a página de contato",
+        "form": contact_form
     }
+    if request.method == "POST":
+        print(request.POST)
     return render(request, "contact/view.html", context)
